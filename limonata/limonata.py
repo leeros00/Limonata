ece55e51dde1b18907b3e8533013d81e2054e4f8
Ross Lee <90817128+leeros00@users.noreply.github.com>
2022-03-30 16:42:16 -0600

    Update limonata.py

--- limonata/limonata.py
+++ limonata/limonata.py
@@ -9,6 +9,9 @@ from .version import __version__
 
 sep = ' '
 
+# TO DO: Potentially update this list to be more inclusive with Arduinos.
+# I could be wrong, but I think the Italians made new VID:PID with the release
+# of those seafoam aquamarine Arduinos
 arduinos = [('USB VID:PID=16D0:0613', 'Arduino Uno'),
             ('USB VID:PID=1A86:7523', 'NHduino'),
             ('USB VID:PID=2341:8036', 'Arduino Leonardo'),
@@ -19,9 +22,13 @@ arduinos = [('USB VID:PID=16D0:0613', 'Arduino Uno'),
 _firmwareurl = https://github.com/leeros00/Limonata/tree/main/Limonata_alpha
 _connected = False
 
-
-def clip(val, lower = 0, upper = 100)
-	"""Limit value to be between lower and upper limits"""
+def clip(val, lower = 0, upper = 100):
 	return max(lower, min(val, upper)
 	
-def 
+def command(val, lower = 0, upper = 100):
+	return name + sep + str(clip(argument, lower, upper))
+			   
+def find_arduino(port = ''):
+	comports = [tuple for tuple in list_ports.comports() if port in tuple[0]]
+			   
+			   
