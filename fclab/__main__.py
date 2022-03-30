87026650a0ed3613fdadc2ee3800e0808ea9b5c7
Ross Lee <90817128+leeros00@users.noreply.github.com>
2022-03-30 10:39:34 -0600

    Add files via upload

---
+++ fclab/__main__.py
@@ -0,0 +1,10 @@
+"""
+Created on Tues March 23  7 15:06:51 2022
+
+@author: ltlee
+"""
+
+from .fclab import diagnose
+
+
+diagnose()
---
+++ fclab/version.py
@@ -0,0 +1,12 @@
+"""
+Created on Tues March 23  7 15:06:51 2022
+
+@author: ltlee
+"""
+
+# Store the version here so:
+# 1) we don't load dependencies by storing it in __init__.py
+# 2) we can import it in setup.py for the same reason
+# 3) we can import it into your module module
+
+__version__ = "0.0.0"
