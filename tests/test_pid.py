8307bc7ddf6c7737d782e973dd4de07c0afc2a05
Ross Lee <rjlee96@gmail.com>
2024-10-14 22:16:49 -0600

    first test

--- tests/test_pid.py
+++ tests/test_pid.py
@@ -1,12 +1,15 @@
 import sys
 import os
 
+import limonata.reactor
+import limonata.reactor.reactor
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
 import numpy as np
 import matplotlib.pyplot as plt
-import tclab
+import limonata
 import time
 
 
@@ -83,46 +86,46 @@ class TestLimonataRed(unittest.TestCase):
 
             n = 600  # Number of second time points (10 min)
             tm = np.linspace(0,n-1,n) # Time values
-            lab = tclab.TCLab()
-            T1 = np.zeros(n)
-            Q1 = np.zeros(n)
+            lab = limonata.reactor.reactor.Reactor()
+            T = np.zeros(n)
+            Q = np.zeros(n)
             # step setpoint from 23.0 to 60.0 degC
             SP1 = np.ones(n)*23.0
             SP1[10:] = 60.0
-            Q1_bias = 0.0
+            Q_bias = 0.0
             ierr = 0.0
             for i in range(n):
                 # record measurement
-                T1[i] = lab.T1
+                T[i] = lab.T
 
                 # --------------------------------------------------
-                # call PID controller function to change Q1[i]
+                # call PID controller function to change Q[i]
                 # --------------------------------------------------
-                [Q1[i],P,ierr,D] = pid(SP1[i],T1[i],T1[max(0,i-1)],ierr,1.0)
+                [Q[i],P,ierr,D] = pid(SP1[i],T[i],T[max(0,i-1)],ierr,1.0)
 
-                lab.Q1(Q1[i])
+                lab.Q(Q[i])
                 if i%20==0:
                     print(' Heater,   Temp,  Setpoint')
-                print(f'{Q1[i]:7.2f},{T1[i]:7.2f},{SP1[i]:7.2f}')
+                print(f'{Q[i]:7.2f},{T[i]:7.2f},{SP1[i]:7.2f}')
                 # wait for 1 sec
                 time.sleep(1)
             lab.close()
             # Save data file
-            data = np.vstack((tm,Q1,T1,SP1)).T
+            data = np.vstack((tm,Q,T,SP1)).T
             np.savetxt('PID_control.csv',data,delimiter=',',\
-                    header='Time,Q1,T1,SP1',comments='')
+                    header='Time,Q,T,SP1',comments='')
 
             # Create Figure
             plt.figure(figsize=(10,7))
             ax = plt.subplot(2,1,1)
             ax.grid()
             plt.plot(tm/60.0,SP1,'k-',label=r'$T_1$ SP')
-            plt.plot(tm/60.0,T1,'r.',label=r'$T_1$ PV')
+            plt.plot(tm/60.0,T,'r.',label=r'$T_1$ PV')
             plt.ylabel(r'Temp ($^oC$)')
             plt.legend(loc=2)
             ax = plt.subplot(2,1,2)
             ax.grid()
-            plt.plot(tm/60.0,Q1,'b-',label=r'$Q_1$')
+            plt.plot(tm/60.0,Q,'b-',label=r'$Q_1$')
             plt.ylabel(r'Heater (%)')
             plt.xlabel('Time (min)')
             plt.legend(loc=1)
